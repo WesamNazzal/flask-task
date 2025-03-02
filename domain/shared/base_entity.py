883136340d8d855c400b99d787a66df8d9c2e700
WesamNazzal <wesam.nazzal@sociumtech.com>
2025-03-02 12:39:31 +0000

    fixing

--- domain/shared/base_entity.py
+++
@@ -1,6 +0,0 @@
-class BaseEntity:
-    next_id: int = 1
-
-    def __init__(self) -> None:
-        self.id: int = BaseEntity.next_id
-        BaseEntity.next_id += 1
--- domain/student.py
+++
@@ -1,9 +0,0 @@
-from domain.shared.base_entity import BaseEntity
-
-
-class Student(BaseEntity):
-    def __init__(self, name: str, age: int, grade: str) -> None:
-        super().__init__()
-        self.name: str = name
-        self.age: int = age
-        self.grade: str = grade
